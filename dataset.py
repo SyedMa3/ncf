067f866668cb33899381eda6e551bc32dc8cd204
SyedMa3 <sammateen10@outlook.com>
2022-07-04 20:40:12 +0530

    fixed bug in dataset.py

--- dataset.py
+++ dataset.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 
-train = pd.read_csv('./drive/MyDrive/ncf/data/train.csv')
-test = pd.read_csv('./drive/MyDrive/ncf/data/test.csv')
+train = pd.read_csv('./data/train.csv')
+test = pd.read_csv('./data/test.csv')
+dataset = pd.read_csv('./data/ratings.csv')
 
 num_users = len(dataset['userId'].unique())
 num_movies = len(dataset['movieId'].unique())
