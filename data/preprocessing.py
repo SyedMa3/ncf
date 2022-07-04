354391f56494133958e90052fea5ae40c5b77949
SyedMa3 <sammateen10@outlook.com>
2022-07-04 16:28:31 +0530

    completed preprocessing

---
+++ data/preprocessing.py
@@ -0,0 +1,22 @@
+index = 0
+
+f1 = open('data/train.csv', 'w')
+f2 = open('data/test.csv', 'w')
+
+with open('data/ratings.csv', 'r') as f:
+    r = f.readline()
+    
+    while r != '':
+        r = f.readline()
+        # print(r)
+        x = r.split(',')
+        if x[0] != index:
+            f2.write(r)
+            index = x[0]
+        else:
+            f1.write(r)
+        # r = r.split(',')
+        # f.write("hello")
+
+f1.close()
+f2.close()
