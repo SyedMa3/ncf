f967376ada142382049278adc65ebaa09530820f
SyedMa3 <sammateen10@outlook.com>
2022-07-05 18:04:44 +0530

    modified dataset.py

--- dataset.py
+++ dataset.py
@@ -1,9 +1,38 @@
 import pandas as pd
 import numpy as np
 
-train = pd.read_csv('./data/train.csv')
-test = pd.read_csv('./data/test.csv')
-dataset = pd.read_csv('./data/ratings.csv')
+data = pd.read_csv('./data/ratings.csv')
+data['sort_latest'] = data.groupby(['userId'])['timestamp'].rank(method='First', ascending=False)
 
-num_users = len(dataset['userId'].unique())
-num_movies = len(dataset['movieId'].unique())
+train = data[data['sort_latest'] != 1]
+test = data[data['sort_latest'] == 1]
+
+
+num_users = len(data['userId'].unique())
+num_movies = len(data['movieId'].unique())
+
+train.loc[:, 'rating'] = 1
+test.loc[:, 'rating'] = 1
+
+all_movieIds = data['movieId'].unique()
+
+users, items, labels = [], [], []
+
+user_item_set = set(zip(train['userId'], train['movieId']))
+
+num_negatives = 4
+
+for (u,i) in user_item_set:
+    users.append(u)
+    items.append(i)
+    labels.append(1)
+
+    for _ in range(num_negatives):
+        negative_item = np.random.choice(all_movieIds)
+
+        while(u, negative_item) in user_item_set:
+            negative_item = np.random.choice(all_movieIds)
+
+        users.append(u)
+        items.append(negative_item)
+        labels.append(0)
